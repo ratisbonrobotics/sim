e4d03d296fed5374df5fb670b003143410f24780
Markus Heimerl <markusheimerl@hotmail.com>
2024-05-28 19:39:48 +0200

    🛠️ depth/ray.py -> Added vray with jax.vmap 🛠️ depth/ray.py -> Updated depth calculation 🛠️ depth/ray.py -> Modified while_loop initial state

--- depth/ray.py
+++ depth/ray.py
@@ -29,6 +29,9 @@ mujoco.mj_resetData(model, data)
 mjx_model = mjx.put_model(model)
 mjx_data = mjx.put_data(model, data)
 
+# vmap mjx.ray
+vray = jax.vmap(mjx.ray, (None, None, None, 0), (0, 0))
+
 # Define sim function
 def sim(mjx_m: mjx.Model, mjx_d: mjx.Data):
     def cond_fun(carry : typing.Tuple[mjx.Model, mjx.Data, typing.Tuple[jax.Array, jax.Array]]):
@@ -38,10 +41,12 @@ def sim(mjx_m: mjx.Model, mjx_d: mjx.Data):
     def body_fun(carry : typing.Tuple[mjx.Model, mjx.Data, typing.Tuple[jax.Array, jax.Array]]):
         mjx_m, mjx_d, depth = carry
         mjx_d = mjx.step(mjx_m, mjx_d)
-        depth = mjx.ray(mjx_m, mjx_d, jax.numpy.array([0.0, 0.0, 0.0], dtype=float), jax.numpy.array([1.0, 1.0, 1.0], dtype=float))
+        origin = jax.numpy.array([0.0, 0.0, 0.0], dtype=float)
+        directions = jax.numpy.array([[1.0, 1.0, 1.0], [1.0, 0.5, 1.0]], dtype=float)
+        depth = vray(mjx_m, mjx_d, origin, directions)
         return mjx_m, mjx_d, depth
 
-    return jax.lax.while_loop(cond_fun, body_fun, (mjx_m, mjx_d, (jax.numpy.zeros((), dtype=float), jax.numpy.zeros((), dtype=int))))
+    return jax.lax.while_loop(cond_fun, body_fun, (mjx_m, mjx_d, (jax.numpy.zeros((2), dtype=float), jax.numpy.zeros((2), dtype=int))))
 
 # simulate
 mjx_m, mjx_d, depth = jax.jit(sim)(mjx_model, mjx_data)
