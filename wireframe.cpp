212518effa5f79753af9d27ecd31becaaa6bfd3c
Markus Heimerl <markusheimerl@hotmail.com>
2024-06-29 21:07:39 +0200

    🛠️ wireframe.cpp -> Removed Model class 🛠️ wireframe.cpp -> Added inline parsing logic 🛠️ wireframe.cpp -> Modified line function signature

--- wireframe.cpp
+++ wireframe.cpp
@@ -15,45 +15,7 @@ public:
     Vec3f(float x = 0, float y = 0, float z = 0) : x(x), y(y), z(z) {}
 };
 
-class Model {
-private:
-    std::vector<Vec3f> verts_;
-    std::vector<std::vector<int>> faces_;
-public:
-    Model(const char *filename) {
-        std::ifstream in;
-        in.open(filename, std::ifstream::in);
-        if (in.fail()) return;
-        std::string line;
-        while (!in.eof()) {
-            std::getline(in, line);
-            std::istringstream iss(line.c_str());
-            char trash;
-            if (!line.compare(0, 2, "v ")) {
-                iss >> trash;
-                Vec3f v;
-                iss >> v.x >> v.y >> v.z;
-                verts_.push_back(v);
-            } else if (!line.compare(0, 2, "f ")) {
-                std::vector<int> f;
-                int itrash, idx;
-                iss >> trash;
-                while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-                    idx--; // in wavefront obj all indices start at 1, not zero
-                    f.push_back(idx);
-                }
-                faces_.push_back(f);
-            }
-        }
-        std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
-    }
-    int nverts() { return (int)verts_.size(); }
-    int nfaces() { return (int)faces_.size(); }
-    std::vector<int> face(int idx) { return faces_[idx]; }
-    Vec3f vert(int i) { return verts_[i]; }
-};
-
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color) {
     bool steep = false;
     if (std::abs(x0-x1)<std::abs(y0-y1)) {
         std::swap(x0, y0);
@@ -85,13 +47,40 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
 int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
-    Model model("african_head.obj");
+    
+    std::vector<Vec3f> verts;
+    std::vector<std::vector<int>> faces;
 
-    for (int i=0; i<model.nfaces(); i++) {
-        std::vector<int> face = model.face(i);
+    std::ifstream in;
+    in.open("african_head.obj", std::ifstream::in);
+    if (in.fail()) return 1;
+    std::string line_str;
+    while (std::getline(in, line_str)) {
+        std::istringstream iss(line_str);
+        char trash;
+        if (!line_str.compare(0, 2, "v ")) {
+            iss >> trash;
+            Vec3f v;
+            iss >> v.x >> v.y >> v.z;
+            verts.push_back(v);
+        } else if (!line_str.compare(0, 2, "f ")) {
+            std::vector<int> f;
+            int itrash, idx;
+            iss >> trash;
+            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
+                idx--; // in wavefront obj all indices start at 1, not zero
+                f.push_back(idx);
+            }
+            faces.push_back(f);
+        }
+    }
+    std::cerr << "# v# " << verts.size() << " f# "  << faces.size() << std::endl;
+
+    for (int i=0; i<faces.size(); i++) {
+        std::vector<int> face = faces[i];
         for (int j=0; j<3; j++) {
-            Vec3f v0 = model.vert(face[j]);
-            Vec3f v1 = model.vert(face[(j+1)%3]);
+            Vec3f v0 = verts[face[j]];
+            Vec3f v1 = verts[face[(j+1)%3]];
             int x0 = (v0.x+1.)*width/2.;
             int y0 = (v0.y+1.)*height/2.;
             int x1 = (v1.x+1.)*width/2.;
